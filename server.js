151d7be29652adbde441584f6bc36a750e2aa0a6
Parker Helland <hellandp@gmail.com>
2019-04-11 15:00:02 -0700

    exported create server to app.js then imported to server

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-const http = require('http');
-const { parse } = require('url');
+const app = require('./lib/app');
 
-// tcp uses SOCKETS
-// request, response
-
-http.createServer((req, res) => {
-  const url = parse(req.url);
-  console.log('We Linked!', url);
-  
-  res.setHeader('Content-Type', 'application/json', 'text/html');
-  switch(url.pathname) {
-    case '/birthday':
-      res.end('Happy Birthday');
-      break;
-    case '/tomorrow':
-      res.end('Tomorrow, Tomorrow');
-      break;
-    case '/home':
-      res.end(JSON.stringify({ name: 'Parker' }));
-      break;
-    default:
-      res.statusCode = 404;
-      res.end('Not Found');
-      break;
-  }
-  
-}).listen(4657, () => { console.log('Parker: server running'); });
+app.listen(4657, () => { console.log('Parker: server running'); });
