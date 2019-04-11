8e91bc6781ea3bbb5ec7de1a50db7743547c543e
Laura Badgley <l.badgleyy@gmail.com>
2019-04-11 16:05:36 -0700

    app birthday test green

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// const http = require('http');
-// const { parse } = require('url');
 const app = require('./lib/app');
 
 app.listen(2121);
-// http.createServer((req, res) => {
-//   const url = parse(req.url);
-//   console.log(url);
-
-//   if(url.pathname === '/birthday') {
-//     res.end('Happy Birthday!');
-//   } else if(url.pathname === '/tomorrow') {
-//     res.end('Tomorrow, tomorrow');
-//   } else if(url.pathname === '/tomorrow/birthday') {
-//     res.end('Tomorrow is your birthday!');
-//   } else if(url.pathname === '/') {
-//     res.statusCode = 404;
-//     res.end('Not Found');
-//   }
-//   res.writeHeader({ 'Content-type': 'application/json' });
-//   res.end('thanks for visiting!');
-//   console.log('Req incoming!');
-// }).listen(2121);
