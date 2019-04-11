45c96c7d4e915ff9e1a800aa756bb1a368f29706
Sean Nelson <smnelsonn@gmail.com>
2019-04-11 14:11:39 -0700

    move server to app/server split

--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 const http = require('http');
 const { parse } = require('url');
+const server = require('./lib/app.js');
 
-http.createServer((req, res) => {
-  const url = (parse(req.url));
-  console.log(url);
+server.listen(7890);
+
+// http.createServer((req, res) => {
+//   const url = (parse(req.url));
+//   console.log(url);
   
-  if(url.pathname === '/birthday'){
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-  <html>
-    <head>
-      <title>Bday</title>
-    </head
-    <body>
-      <h1> Happy Bday</h1>
-    </body>
-  </html>`);
-  }
+//   if(url.pathname === '/birthday'){
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end(`
+//   <html>
+//     <head>
+//       <title>Bday</title>
+//     </head
+//     <body>
+//       <h1> Happy Bday</h1>
+//     </body>
+//   </html>`);
+//   }
 
-  if(url.pathname === '/tomorrow'){
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-  <html>
-    <head>
-      <title>Tomorrow</title>
-    </head
-    <body>
-      <h1> Tomorrow</h1>
-    </body>
-  </html>`);
-  }
-  if(url.pathname === '/birthday/tomorrow'){
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-  <html>
-    <head>
-      <title>Tomorrow Birthday</title>
-    </head
-    <body>
-      <h1> Tomorrow is your birthday!</h1>
-    </body>
-  </html>`);
-  }
-}).listen(7890);
+//   if(url.pathname === '/tomorrow'){
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end(`
+//   <html>
+//     <head>
+//       <title>Tomorrow</title>
+//     </head
+//     <body>
+//       <h1> Tomorrow</h1>
+//     </body>
+//   </html>`);
+//   }
+//   if(url.pathname === '/birthday/tomorrow'){
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end(`
+//   <html>
+//     <head>
+//       <title>Tomorrow Birthday</title>
+//     </head
+//     <body>
+//       <h1> Tomorrow is your birthday!</h1>
+//     </body>
+//   </html>`);
+//   }
+// }).listen(7890);
