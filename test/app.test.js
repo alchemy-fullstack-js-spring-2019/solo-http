faea39a9141a0d99a6a034f837792f25e6a7a288
mcnadams <mcneilbo@oregonstate.edu>
2019-04-11 14:51:55 -0700

    supertest app  route

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+    it('responds to the birthday route', () => {
+        return request(app)
+            .get('/birthday')
+            .then(res => {
+                expect(res.text).toEqual('Happy birthday');
+            });
+    });
+});
