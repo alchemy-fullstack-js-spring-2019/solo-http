5be1b26c8fbc0d8bc3606de34bf4e75a7e56f19a
mcnadams <mcneilbo@oregonstate.edu>
2019-04-11 15:40:32 -0700

    query strings app

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../lib/app');
 const app2 = require('../lib/jsonapp');
+const app3 = require('../lib/queryapp');
 
 describe('app routes', () => {
     it('responds to the birthday route', () => {
@@ -15,6 +16,13 @@ describe('app routes', () => {
             .get('/tester')
             .then(res => {
                 expect(JSON.parse(res.text)).toEqual({ testing: 123 });
+            });
+    });
+    it.only('app3 responds to the name query', () => {
+        return request(app3)
+            .get('/you?name=ryan')
+            .then(res => {
+                expect(JSON.parse(res.text)).toEqual({ text: 'hi there ryan' });
             });
     });
 });
