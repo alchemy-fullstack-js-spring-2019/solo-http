9796e605c2c77c6450beb3ea5a27a90d42093d75
mcnadams <mcneilbo@oregonstate.edu>
2019-04-11 15:00:51 -0700

    json app

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const app2 = require('../lib/jsonapp');
 
 describe('app routes', () => {
     it('responds to the birthday route', () => {
@@ -7,6 +8,13 @@ describe('app routes', () => {
             .get('/birthday')
             .then(res => {
                 expect(res.text).toEqual('Happy birthday');
+            });
+    });
+    it('app2 responds to the tester route', () => {
+        return request(app2)
+            .get('/tester')
+            .then(res => {
+                expect(JSON.parse(res.text)).toEqual({ testing: 123 });
             });
     });
 });
