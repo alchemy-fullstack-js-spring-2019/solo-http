25a40d981a8a03ddd9fb5023503bc1f67b2ea7de
Mallarie <mallariemilano@gmail.com>
2019-04-11 15:49:01 -0700

    sending client html from server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   console.log('Request incoming!');
+  res.setHeader('Content-Type', 'text/html');
   res.end(`
   <html>
     <head>
@@ -10,5 +11,6 @@ http.createServer((req, res) => {
     <body>
       <h1>Thanks for visiting!</h1>
     <body>
-    `);
+  </html>
+  `);
 }).listen(7890);
