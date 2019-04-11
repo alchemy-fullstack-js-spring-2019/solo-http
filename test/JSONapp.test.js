f73c4b6580e50f2ae691814f607b0e0de72fbae2
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-11 15:28:27 -0700

    added app and app.test because I was missing them

--- test/JSONapp.test.js
+++ test/JSONapp.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
-const app = require('../lib/app');
+const app = require('../lib/JSONapp');
 
 describe('app routes', () => {
   it('responds to the testing route', () => {
     return request(app)
       .get('/tester')
       .then(res => {
-        expect(res.text).toEqual('testing123');
+        expect(res.text).toEqual('{"testing":123}');
       });
   });
 });
