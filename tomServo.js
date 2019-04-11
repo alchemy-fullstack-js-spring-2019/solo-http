b3172537850684f55554dc400e92d62cdc8512e0
Masmics <cherlocker3113@gmail.com>
2019-04-11 16:37:36 -0700

    starting query strings

---
+++ tomServo.js
@@ -0,0 +1,13 @@
+const { app } = require('./lib/app');
+
+app.listen(7891);
+
+// Create an app.js file
+// export an http listener function ((req, res) => {})
+// respond to /tester with "testing123"
+// create a server.js file
+// const app = require('./lib/app')
+// use createServer to create an http server with app
+// use listen to start server on a port
+// tests
+// use supertest to to test your app
