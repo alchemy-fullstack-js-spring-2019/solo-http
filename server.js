0b1ed33159ae9b5216213d2164170373469f4cf8
Payton Lower <paytown@me.com>
2019-04-11 14:35:21 -0700

    working with switch

--- server.js
+++ server.js
@@ -6,24 +6,86 @@ http.createServer((req, res) => {
   console.log(url);
   console.log('Connected');
 
-if(url.pathname === '') {
-  // respond one wy
-} else if(url.pathname = '') {
-  // other wy
-} else {
-  // redirect 404
-}
 
-  res.setHeader('Content-Type', 'text-html');
-  res.end(`
-  <html>
-    <head>
-      <title>HTML AGAIN THANK GOD</title>
-    </head>
-    <body>
-      <h1>I miss you, HTML</h1>
-      <h6>Bye :(</h6>
-    </body>
-  </html>
-  `);
+  const pattern = url.pathname;
+  switch(pattern) {
+    case '/birthday': 
+      res.setHeader('Content-Type', 'text-html');
+      res.end(`
+      <html>
+        <head>
+          <title>HBD</title>
+        </head>
+        <body>
+          <h1>HAPPY BIRTHDAY</h1>
+        </body>
+      </html>
+      `);
+      break;
+    case '/tomorrow': 
+      res.setHeader('Content-Type', 'text-html');
+      res.end(`
+        <html>
+          <head>
+            <title>Calm down</title>
+          </head>
+          <body>
+            <h1>It's not your birthday yet</h1>
+          </body>
+        </html>
+        `);
+      break;
+    default:
+      res.setHeader('Content-Type', 'text-html');
+      res.end(`
+      <html>
+        <head>
+          <title>Nope</title>
+        </head>
+        <body>
+          <h1>Don't look here</h1>
+        </body>
+      </html>
+      `);
+      break;
+  }
+
+  // if(url.pathname === '/birthday') {
+  //   res.setHeader('Content-Type', 'text-html');
+  //   res.end(`
+  //   <html>
+  //     <head>
+  //       <title>HBD</title>
+  //     </head>
+  //     <body>
+  //       <h1>HAPPY BIRTHDAY</h1>
+  //     </body>
+  //   </html>
+  //   `);
+  // } else if(url.pathname === '/tomorrow') {
+  //   res.setHeader('Content-Type', 'text-html');
+  //   res.end(`
+  //   <html>
+  //     <head>
+  //       <title>Calm down</title>
+  //     </head>
+  //     <body>
+  //       <h1>It's not your birthday yet</h1>
+  //     </body>
+  //   </html>
+  //   `);
+  // } else {
+  //   res.status(404).end();
+  // res.setHeader('Content-Type', 'text-html');
+  // res.end(`
+  // <html>
+  //   <head>
+  //     <title>Nope</title>
+  //   </head>
+  //   <body>
+  //     <h1>Don't look here</h1>
+  //   </body>
+  // </html>
+  // `);
+  // }
 }).listen(6969);
