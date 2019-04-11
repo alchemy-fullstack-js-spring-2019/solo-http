6b19079a454837643c9220100ec2e84361737be3
Tommy Tran <tranttommy95@gmail.com>
2019-04-11 15:27:10 -0700

    refactored app and 'query strings' section test passed

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,19 @@ const app = require('../lib/app.js');
 const request = require('supertest');
 
 describe('app routes', () => {
-  it('it responds to a path', () => {
+  it('responds to a path', () => {
     return request(app)
       .get('/tester')
       .then(res => {
         expect(res.text).toBe('testing123');
+      });
+  });
+
+  it('responds with an object based on query string', () => {
+    return request(app)
+      .get('/you?name=tommy')
+      .then(res => {
+        expect(res.body).toEqual({ text: 'hi there tommy' });
       });
   });
 });
