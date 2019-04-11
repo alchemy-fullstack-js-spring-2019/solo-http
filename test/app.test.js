315a110f732bf1f1f31c4a3e42dcb7421fd4a48d
mcnadams <mcneilbo@oregonstate.edu>
2019-04-11 16:23:48 -0700

    rick and morty app TDD

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const app2 = require('../lib/jsonapp');
 const app3 = require('../lib/queryapp');
+const rmapp = require('../lib/rmapp');
 
 describe('app routes', () => {
     it('responds to the birthday route', () => {
@@ -11,6 +12,7 @@ describe('app routes', () => {
                 expect(res.text).toEqual('Happy birthday');
             });
     });
+
     it('app2 responds to the tester route', () => {
         return request(app2)
             .get('/tester')
@@ -18,11 +20,24 @@ describe('app routes', () => {
                 expect(JSON.parse(res.text)).toEqual({ testing: 123 });
             });
     });
-    it.only('app3 responds to the name query', () => {
+
+    it('app3 responds to the name query', () => {
         return request(app3)
             .get('/you?name=ryan')
             .then(res => {
                 expect(JSON.parse(res.text)).toEqual({ text: 'hi there ryan' });
+            });
+    });
+
+    it('rmapp responds to the name query', () => {
+        return request(rmapp)
+            .get('/character/12')
+            .then(res => {
+                expect(JSON.parse(res.text)).toEqual({ 
+                    'name': 'Alexander',
+                    'status': 'Dead',
+                    'species': 'Human' 
+                });
             });
     });
 });
