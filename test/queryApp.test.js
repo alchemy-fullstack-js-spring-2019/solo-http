05ae4d8c998efe06ca3e67e215104b9ded69455b
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-11 16:06:08 -0700

    query with no name test/passing

--- test/queryApp.test.js
+++ test/queryApp.test.js
@@ -10,4 +10,12 @@ describe('query app routes', () => {
         expect(res.text).toEqual('hello there megan');
       });
   });
+  it('responds to the query string - no name', () => {
+    const url = '/you?name=';
+    return request(queryApp)
+      .get(url)
+      .then(res => {
+        expect(res.text).toEqual('hey');
+      });
+  });
 });
