8d026a8ce829df2abf4e174cd4a3403d96c23a08
Payton Lower <paytown@me.com>
2019-04-11 15:44:00 -0700

    nodemon and tests working

---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const { app } = require('../lib/app');
+
+describe('app route', () => {
+  it('birthday route working', () => {
+    return request(app)
+      .get('/birthday')
+      .then(res => {
+        const html = `
+      <html>
+        <head>
+          <title>HBD</title>
+        </head>
+        <body>
+          <h1>HAPPY BIRTHDAY</h1>
+        </body>
+      </html>
+      `;
+        expect(res.text).toEqual(html);
+      });
+
+  });
+});
