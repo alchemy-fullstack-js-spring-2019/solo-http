7a4f69c873ae2bd69d99f49470e243369a15bf94
Leslie Shea <leslieyshea@gmail.com>
2019-04-11 14:49:23 -0700

    separated app and server into own modules

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-const http = require('http');
-const { parse } = require('url');
+const app = require('./lib/app');
 
-// 200 - ok
-// 204 - ok but no content
-// 301 - redirect
-// 304 - redirect
-// 400 - bad request
-// 401 - unauthorized
-// 403 - forbidden
-// 404 - not found
-// 500 - internal server error
-// 503 - timeout
-
-
-http.createServer((req, res) => {
-  const url = parse(req.url);
-  console.log(url);
-
-  if(url.pathname === '/birthday') {
-    res.end('Happy Birthday!');
-  } else if(url.pathname === '/tomorrow') {
-    res.end('Tomorrow, tomorrow.');
-  } else if(url.pathname === '/birthday/tomorrow') {
-    res.end('Tomorrow is your birthday.');
-  } else {
-    res.end('404 Not found');
-  }
-}).listen(7890);
+app.listen(7890);
