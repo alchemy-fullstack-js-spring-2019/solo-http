2e51adf3f67a3a2ece142beffcc00cd03ccd5343
ollinieminen <otn987@gmail.com>
2019-04-11 14:44:52 -0700

    create app.test.js

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-
+const app = require('./lib/app');
+app.listen(8900);
 const http = require('http');
-const { parse } = require('url');
+http.createServer(app).listen(8900);
 
-http.createServer((req, res) => {
-  const url = parse(req.url);
-  if(url.path === '/birthday') {
-    res.end('Happy Birthday!!!!');
-  }
-  if(url.path === '/tomorrow') {
-    res.end('Tomorrow Tomorrow');
-  }
-  if(url.path === '/birthday/tomorrow') {
-    res.end('Tomorrow birthday Tomorrow');
-  }
-  res.end('go to/birthday or/tomorrow for a present');
-}).listen(7890);
