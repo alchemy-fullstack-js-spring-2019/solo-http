a61451a2ec753bec7ad436f6865dc789fd894a8c
Ben Parisot <me@benparisot.com>
2019-04-11 14:17:48 -0700

    write switch statements for server pathnames

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const http = require('http');
-
+const { parse } = require('url');
 //Create a server
 http.createServer((req, res) => {
   console.log('request incoming!');
+  const url = parse(req.url);
+  console.log(url);
+
+  switch(url.pathname) {
+    case '/birthday':
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><span style="color:red">Happy Birthday Fren</span></body></html>');
+      break;
+    
+    case '/json':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.toString({ 'firstName': 'Duckface', 'lastName': 'McLiverneck' }));
+      break;
+
+    case '/waterfalls':
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><span style="color:blue">| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>****</span></body></html>');
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.end(`Sorry buuuuut....:${res.statusCode}`);
+      break;
+  }
 })
   .listen(7890);
 
