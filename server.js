ff0fcc1fac50edb9f70cec60cbe2d844890218f3
Mallarie <mallariemilano@gmail.com>
2019-04-11 16:37:02 -0700

    parsing url object to terminal

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const http = require('http');
+const { parse } = require('url');
 
 http.createServer((req, res) => {
-  console.log('Request incoming!');
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`
-  <html>
-    <head>
-      <title>Hi</title>
-    </head>
-    <body>
-      <h1>Thanks for visiting!</h1>
-    <body>
-  </html>
-  `);
+  const url = parse(req.url);
+  console.log(url);
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ name: 'Mal' }));
 }).listen(7890);
