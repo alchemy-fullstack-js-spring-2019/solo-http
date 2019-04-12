b1c6b3e7a2a535ef6ddba41d2cc1d76a83f4d51f
Cara Ponzini <ponzini@gmail.com>
2019-04-12 14:47:44 -0700

    updated tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,42 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../lib/app');
+const People = require('../lib/models/People');
+
 
 describe('app routes', () => {
-  it('tester pathname resolves ', () => {
-    return request(app).get('/tester')
-      .then(res => expect(res.body).toEqual({ testing: '123' }));
+  afterAll(() => {
+    return People.drop();
   });
 
-  it('resolves to object', () => {
-    return request(app).get('/you?name=cara')
-      .then(res => expect(res.body).toEqual({ text: 'hi there cara' }));
+  test('it adds a person', () => {
+    const person = {
+      name: 'cara',
+      age: '35',
+      color: 'black'
+    };
+    return request(app)
+      .post('/people').send(person)
+      .then(res => {
+        expect(res.body).toEqual({ ...person, _id: expect.any(String) });
+      });
   });
+
+  test('gets a list of all people with /people', () => {
+    return request(app)
+      .get('/people')
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  // it('tester pathname resolves ', () => {
+  //   return request(app).get('/tester')
+  //     .then(res => expect(res.body).toEqual({ testing: '123' }));
+  // });
+
+  // it('resolves to object', () => {
+  //   return request(app).get('/you?name=cara')
+  //     .then(res => expect(res.body).toEqual({ text: 'hi there cara' }));
+  // });
 });
 
