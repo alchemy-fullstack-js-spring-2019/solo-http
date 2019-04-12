766562fbc6124bb775035193aabe1dfa975a5aac
emily baier <emilyrbaier@gmail.com>
2019-04-12 14:52:34 -0700

    GET people and people by id routes tested and working

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const People = require('../lib/models/People');
 
 
 describe('people creates a new person', ()=> {
+    afterAll(() => {
+        return People.drop();
+    });
+
     it('POST/people creates a new person in the database', ()=> {
         return request(app)
             .post('/people')
@@ -13,6 +18,27 @@ describe('people creates a new person', ()=> {
                     age: 35,
                     color: 'green',
                     _id: expect.any(String)
+                });
+            });
+    });
+    it('GET/people returns a list of people in database', ()=> {
+        return request(app)
+            .get('/people')
+            .then(res => {
+                expect(res.body).toHaveLength(1);
+            });
+    });
+    it('GET/people can return a person by id', ()=> {
+        People.create({ name: 'ben', age: 39, color: 'purple' })
+            .then(createdPerson => {
+                return request(app)
+                    .get(`/people/${createdPerson._id}`);
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'ben',
+                    age: 39,
+                    color: 'purple'
                 });
             });
     });
