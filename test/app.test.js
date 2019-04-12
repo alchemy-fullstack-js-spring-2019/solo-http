892793d5922103776d7d4c0c216fef479f0f947a
Peter Kotowski <bighoopee@gmail.com>
2019-04-12 09:05:19 -0700

    fixing errors

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,22 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock()
+
 describe('app routes', () => {
-  it('responds to the birthday route', () => {
+//   it('responds to the birthday route', () => {
+//     return request(app)
+//       .get('/birthday')
+//       .query({ name: 'me' })
+//       .then(res => {
+//         expect(res.text).toEqual('happy birthday');
+//       });
+//   });
+  it('returns a character', () => 
+  {
     return request(app)
-      .get('/birthday')
-      .query({ name: 'me' })
-      .then(res => {
-        expect(res.text).toEqual('happy birthday');
-      });
+      .get('/character/1')
+      .then(res => console.log(res.body));
+      
   });
 });
