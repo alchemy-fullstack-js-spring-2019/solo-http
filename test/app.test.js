ab11944892583d79a5c0d713942d512728680617
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-12 11:45:56 -0700

    post test passing, added before and after each fns

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,29 @@
 const request = require('supertest');
 const app = require('../lib/app.js');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+// const Person = require('../lib/models/People');
 
 
-describe('something', () => {
-  it('responds to tester route', () => {
-    
+describe('People API tests', () => {
+  beforeEach(() => {
+    mkdirp('./data/people');
+  });
+  afterEach(done => {
+    rimraf('./data/people/', done);
+  });
+  it('POST method creates new person and return json', () => {
+    return request(app)
+      .post('/people')
+      .send({ name: 'Marty', age: 28, class: 'wizard' })
+      .then(res => {
+        expect(JSON.parse(res.text)).toEqual({
+          name: 'Marty',
+          age: 28,
+          class: 'wizard',
+          _id: expect.any(String)
+        });
+      });
   });
 });
 
