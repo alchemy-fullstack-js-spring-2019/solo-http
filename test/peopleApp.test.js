805371efa3f4e3699905aac1182046c18ade3483
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-12 13:23:37 -0700

    saving a person test written and passing

---
+++ test/peopleApp.test.js
@@ -0,0 +1,40 @@
+const app = require('../lib/peopleApp');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const Store = require('../lib/models/Store');
+const fsPromises = require('fs').promises;
+const People = require('../lib/models/People');
+const uuid = require('uuid/v4');
+
+// const rootDirectory = path.join(_dirname, '../', 'people');
+
+let people;
+
+describe('people app', () => {
+  beforeAll(done => {
+    mkdirp('./testData/people', done);
+  });
+  beforeEach(() => {
+    people = new Store('./testData/people');
+  });
+  // beforeEach(done => {
+  //   people.drop(done);
+  // });
+  afterAll(done => {
+    rimraf('./testData', done);
+  });
+
+  it('saves a person', () => {
+    return people.create({ 
+      name: 'patrick', 
+      age: 35, 
+      color: 'blue',
+      _id: uuid() 
+    })
+      .then(createdPerson => {
+        expect(createdPerson).toEqual({ name: 'patrick', age: 35, color: 'blue', _id: expect.any(String) });
+      });   
+  });
+});
+
