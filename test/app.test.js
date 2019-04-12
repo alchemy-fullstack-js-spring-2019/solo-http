6a7710185dd01ce51098669ea1219eb5ac84a3fa
Cheri Portman <cheriportman@hotmail.com>
2019-04-12 11:48:31 -0700

    preparing for today's lab tasks

--- test/app.test.js
+++ test/app.test.js
@@ -2,33 +2,33 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it('responds to the birthday route', () => {
-    return request(app)
-      .get('/birthday')
-      .then(res => {
-        expect(res.text).toEqual('Happy Birthday');
-      });
-  });
-  it('handles a json response', () => {
-    return request(app)
-      .get('/tester')
-      .then(res => {
-        expect(res.body).toEqual({ name: 'stitch' });
-      });
-  });
-  it('handles a query string', () => {
-    return request(app)
-      .get('/you')
-      .query({ name: 'cheri' })
-      .then(res => {
-        expect(res.body).toEqual({ text: 'hello cheri' });
-      });
-  });
-  it.only('gets a particular character from an api', () => {
-    return request(app)
-      .get('/character/2')
-      .then(res => {
-        expect(res.body).toEqual('Morty Smith');
-      });
-  });
+  // it('responds to the birthday route', () => {
+  //   return request(app)
+  //     .get('/birthday')
+  //     .then(res => {
+  //       expect(res.text).toEqual('Happy Birthday');
+  //     });
+  // });
+  // it('handles a json response', () => {
+  //   return request(app)
+  //     .get('/tester')
+  //     .then(res => {
+  //       expect(res.body).toEqual({ name: 'stitch' });
+  //     });
+  // });
+  // it('handles a query string', () => {
+  //   return request(app)
+  //     .get('/you')
+  //     .query({ name: 'cheri' })
+  //     .then(res => {
+  //       expect(res.body).toEqual({ text: 'hello cheri' });
+  //     });
+  // });
+  // it.only('gets a particular character from an api', () => {
+  //   return request(app)
+  //     .get('/character/2')
+  //     .then(res => {
+  //       expect(res.body).toEqual('Morty Smith');
+  //     });
+  // });
 });
