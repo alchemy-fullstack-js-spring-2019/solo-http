70cac28d2870dbe8f5a0dc7e2d09407be96358d5
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-12 16:55:01 -0700

    rapid fire coding following Ryan

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,7 @@ describe('People API tests', () => {
   // beforeAll(done => {
   //   mkdirp('./data/people', done);
   // });
-  afterAll(() => {
+  afterEach(() => {
     return People.drop();
   });
   it('POST method (/people) creates new person and return json', () => {
@@ -18,6 +18,61 @@ describe('People API tests', () => {
         expect(res.body).toEqual({
           name: 'test',
           _id: expect.any(String)
+        });
+      });
+  });
+  it('gets a list of people with the /people route', () => {
+    return People.create({
+      name: 'tester'
+    })
+      .then(() => {
+        return request(app)
+          .get('/people');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body).toContainEqual({
+          name: 'tester',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('gets a person by id', () => {
+    return People.create({ name: 'tester' })
+      .then(person => {
+        return request(app)
+          .get(`/people/${person._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'tester',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('updates a person by ID', () => {
+    return People.create({ name: 'testrr' })
+      .then(person => {
+        return request(app)
+          .put(`/people/${person._id}`)
+          .send({ name: 'tester'});
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'tester',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('deletes a person by id', () => {
+    return People.create({ name: 'tester' })
+      .then(person => {
+        return request(app)
+          .delete(`/people/${person._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
         });
       });
   });
