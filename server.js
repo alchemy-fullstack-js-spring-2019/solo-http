548cf5953a226f1563baa1a72bd1c2481c31b8f4
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-12 11:20:51 -0700

    setting up for Fridays solo, lots of Ryan code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./lib/app.js');
-const appJson = require('./lib/app.json.js');
+// const appJson = require('./lib/app.json.js');
 
 
 app.listen(7890);
-appJson.listen(4567);
+// appJson.listen(4567);
--- test/app.test.js
+++ test/app.test.js
@@ -4,12 +4,7 @@ const app = require('../lib/app.js');
 
 describe('something', () => {
   it('responds to tester route', () => {
-    return request(app)
-      .get('/tester')
-      // .query({ name: 'ryan'})
-      .then(res => {
-        expect(res.text).toEqual('testing123');
-      });
+    
   });
 });
 
