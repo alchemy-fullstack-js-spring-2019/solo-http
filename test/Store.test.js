74fb0177210f78cd4606bb4fae7c24a162d065f8
Sean Nelson <smnelsonn@gmail.com>
2019-04-12 11:35:16 -0700

    caught up my huge convoluted project to Ryan's + my own past work

--- test/Store.test.js
+++ test/Store.test.js
@@ -2,7 +2,7 @@ const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 const Store = require('../lib/Store');
 
-describe.skip('Store', () => {
+describe('Store', () => {
   let store = null;
 
   beforeAll(done => {
@@ -13,8 +13,8 @@ describe.skip('Store', () => {
     store = new Store('./testData/store');
   });
 
-  beforeEach(done => {
-    store.drop(done);
+  beforeEach(() => {
+    return store.drop();
   });
 
   afterAll(done => {
@@ -23,35 +23,47 @@ describe.skip('Store', () => {
 
   it('creates an object in my store', () => {
     return store.create({ name: 'ryan' })
-      .then(result =>
-        expect(result).toEqual({ name: 'ryan', _id: expect.any(String) }));
+      .then(createdPerson => {
+        expect(createdPerson).toEqual({ name: 'ryan', _id: expect.any(String) });
+      });
   });
 
   it('finds an object by id', () => {
     return store.create({ name: 'uncle bob' })
-      .then(createdUncle =>
-        store.findById(createdUncle._id)
-          .then(foundUncle =>
-            expect(foundUncle).toEqual({ name: 'uncle bob', _id: createdUncle._id })
-          )
-      );
+      .then(createdUncle => {
+        return Promise.all([
+          Promise.resolve(createdUncle),
+          store.findById(createdUncle._id)
+        ]);
+      })
+      .then(([createdUncle, foundUncle]) => {
+        expect(foundUncle).toEqual(createdUncle);
+      });
   });
 
   it('find all objects tracked by the store', () => {
-    Promise.all(
-      [...Array(5)]
-        .map((_, item) => ({
-          item
-        }))
-        .map(item => store.create(item)))
+    // [undefined, undefined, undefined, undefined, undefined]
+    // [{ item: 0 }, { item: 1 }, { item: 2 },  { item: 3 }, { item: 4 }]
+    // [store.create({ item: 0 }), store.create({ item: 1 }, store.create({ item: 2 }, store.create({ item: 3 }, store.create({ item: 4 }, store.create({ item: 5 }]
+    const undefinedArray = [...Array(5)];
+    const arrayOfItems = undefinedArray.map((_, index) => {
+      return {
+        item: index
+      };
+    });
+    const createPromises = arrayOfItems
+      .map(item => store.create(item));
+
+    return Promise.all(createPromises)
       .then(items => {
         return Promise.all([
           Promise.resolve(items),
           store.find()
         ]);
       })
-      .then(([items, foundItems]) =>{
+      .then(([items, foundItems]) => {
         const [item1, item2, item3, item4, item5] = items;
+        expect(foundItems).toHaveLength(5);
         expect(foundItems).toContainEqual(item1);
         expect(foundItems).toContainEqual(item2);
         expect(foundItems).toContainEqual(item3);
@@ -59,7 +71,6 @@ describe.skip('Store', () => {
         expect(foundItems).toContainEqual(item5);
       });
   });
-  
 
   it('deletes an object with an id', () => {
     return store.create({ item: 'I am going to delete' })
@@ -78,27 +89,15 @@ describe.skip('Store', () => {
       });
   });
 
-  it('updates an existing object', done => {
-    return store.create({ name: 'saen' })
+  it('updates an existing object', () => {
+    return store.create({ name: 'rayn' })
       .then(createdItem => {
         return store.findByIdAndUpdate(createdItem._id, {
-          name: 'sean'
+          name: 'ryan'
         });
       })
       .then(updatedItem => {
-        expect(updatedItem.name).toEqual('sean');
+        expect(updatedItem.name).toEqual('ryan');
       });
-
-    // store.create({ name: 'rayn' }, (err, typoCreated) => {
-    //   store.findByIdAndUpdate(typoCreated._id, { name: 'ryan' }, (err, updatedWithoutTypo) => {
-    //     expect(err).toBeFalsy();
-    //     expect(updatedWithoutTypo).toEqual({ name: 'ryan', _id: typoCreated._id });
-    //     store.findById(typoCreated._id, (err, foundObj) => {
-    //       expect(foundObj).toEqual(updatedWithoutTypo);
-    //       done();
-    //     });
-
-    //   });
-    // });
   });
 });
