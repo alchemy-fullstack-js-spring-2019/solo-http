e1f8cb7b70bbca8ee1f260c700a65d77a8d0ca9d
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-12 15:15:02 -0700

    been a while since a commit....

--- test/app.test.js
+++ test/app.test.js
@@ -1,28 +1,65 @@
 const request = require('supertest');
 const app = require('../lib/app.js');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
-// const Person = require('../lib/models/People');
+const People = require('../lib/models/People');
 
 
 describe('People API tests', () => {
-  beforeEach(() => {
-    mkdirp('./data/people');
+  // beforeAll(done => {
+  //   mkdirp('./data/people', done);
+  // });
+  afterAll(() => {
+    return People.drop();
   });
-  afterEach(done => {
-    rimraf('./data/people/', done);
-  });
-  it('POST method creates new person and return json', () => {
+  it('POST method (/people) creates new person and return json', () => {
     return request(app)
       .post('/people')
       .send({ name: 'Marty', age: 28, class: 'wizard' })
       .then(res => {
-        expect(JSON.parse(res.text)).toEqual({
+        expect(res.body).toEqual({
           name: 'Marty',
           age: 28,
           class: 'wizard',
           _id: expect.any(String)
         });
+      });
+  });
+
+  it('GET method (/people) returns a list of all people', () => {
+    People.create({ name: 'Ipa', age: 19, class: 'rogue' });
+    People.create({ name: 'Deborah', age: 42, class: 'bard' });
+    return request(app)
+      .get('/people')
+      .then(res => {
+        expect(res.body).toHaveLength(3);
+        expect(res.body).toContainEqual({ name: 'Marty', age: 28, class: 'wizard', _id: expect.any(String) });
+        expect(res.body).toContainEqual({ name: 'Ipa', age: 19, class: 'rogue', _id: expect.any(String) });
+        expect(res.body).toContainEqual({ name: 'Deborah', age: 42, class: 'bard', _id: expect.any(String) });
+      });
+  });
+  it('GET method (/people/:id/) returns single person', () => {
+    People.create({ name: 'Ginny', age: 22, class: 'druid' })
+      .then(createdPerson => {
+        return request(app)
+          .get(`/people/${createdPerson._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ name: 'Ginny', age: 22, class: 'druid', _id: expect.any(String) });
+      });
+  });
+  it('PUT method (/people/:id) updates a person with specified id and returns update', () => {
+    People.create({ name: 'Frefory', age: 15, class: 'barbarian' })
+      .then(createdPerson => {
+        return request(app)
+          .put(`/people/${createdPerson._id}`)
+          .send({ name: 'Gregory', age: 18, class: 'monk' })
+          .then(res => {
+            expect(res.body._id).toEqual(createdPerson._id);
+            expect(res.body).toEqual({
+              name: 'Gregory',
+              age: 18,
+              class: 'monk'
+            });
+          });
       });
   });
 });
