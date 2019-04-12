a9b527428eab8993390b038eaccbb07d1e4468d6
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-11 22:43:10 -0700

    trying to figure out what is wrong

--- test/services/rickAndMortyApp.test.js
+++ test/services/rickAndMortyApp.test.js
@@ -1,14 +1,14 @@
 const app = require('../../lib/rickAndMortyApp');
 const request = require('supertest');
 
-jest.mock('../../lib/services/rickAndMortyCharacter');
+jest.mock('../../lib/services/rickAndMortyCharacter.js');
 
 describe('rick and morty character', () => {
   it('gets a character from the rick and morty API', () => {
     return request(app)
-      .get('/character/2')
+      .get('/character/3')
       .then(res => {
-        expect(res.text).toEqual({
+        expect(res.body).toEqual({
           name: 'Morty Smith',
           species: 'Human',
           status: 'Alive'
