e4e253c7923d9e749085c72f5314aa79b082beec
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 14:43:42 -0700

    fixing format

--- test/people-app.test.js
+++ test/people-app.test.js
@@ -10,7 +10,11 @@ describe('People routes', () => {
   it('creates a person with /people', () => {
     return request(peopleApp)
       .post('/people')
-      .send({ name: 'leslie', age: 25, color: 'blue' })
+      .send({
+        name: 'leslie',
+        age: 25,
+        color: 'blue'
+      })
       .then(res => {
         expect(res.body).toEqual({
           name: 'leslie',
