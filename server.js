6133a1dc18dcf97590389b90da5f285ca3776170
UncleJerry <jared.myhrberg@gmail.com>
2019-04-12 13:39:54 -0700

    CRUD funcitons working, but have no tests

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
 const app = require('./lib/app');
-const rickMorty = require('./lib/service/rick-morty');
 
 app.listen(5500);
-rickMorty.listen(7890);
-
-
-
-
-
-
---
+++ test/People.test.js
@@ -0,0 +1,13 @@
+const app = require('../lib/app');
+
+describe('testing people db', () => {
+
+  beforeEach(() => {
+    app.listen(5500);
+  });
+
+
+  it('testing create person', () => {
+    expect()
+  });
+});
