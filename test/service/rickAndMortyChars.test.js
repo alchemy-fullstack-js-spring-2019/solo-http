66fb98cac282f65e9f2a61f8f88e41e8ae269068
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-11 20:03:55 -0700

    proper test for rAM app

--- test/service/rickAndMortyChars.test.js
+++ test/service/rickAndMortyChars.test.js
@@ -1,19 +1,21 @@
-const { getCharacter } = require('../../lib/service/getCharacter');
+const app = require('../../lib/rickAndMortyChars');
+const request = require('supertest');
 
 jest.mock('../../lib/service/getCharacter.js');
 
 describe('Rick and Morty API test', () => {
   it('getCharacter takes an id and returns name, status and species', () => {
-    const id = 2;
+    const id = 3;
     const expected = {
       name: 'Morty Smith',
       status: 'Alive',
       species: 'Human'
     };
 
-    return getCharacter(id)
+    return request(app)
+      .get(`/character/${id}`)
       .then(res => {
-        expect(res).toEqual(expected);
+        expect(res.body).toEqual(expected);
       });
   });
 });
