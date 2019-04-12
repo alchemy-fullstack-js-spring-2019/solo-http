21d6564e60a5caa8f58964250713452513805960
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 16:33:17 -0700

    finished CRUD

--- test/people-app.test.js
+++ test/people-app.test.js
@@ -2,42 +2,43 @@ const request = require('supertest');
 const peopleApp = require('../lib/people-app');
 const People = require('../lib/models/People');
 
-describe('People routes', () => {
-  afterAll(() => {
+describe('People route', () => {
+  afterEach(() => {
     return People.drop();
   });
 
-  it('creates a person with /people', () => {
+  it('creates a person with /people route', () => {
     return request(peopleApp)
       .post('/people')
-      .send({
-        name: 'leslie',
-        age: 25,
-        color: 'blue'
-      })
+      .send({ name: 'tester' }) // the data you want to send
       .then(res => {
         expect(res.body).toEqual({
-          name: 'leslie',
-          age: 25,
-          color: 'blue',
+          name: 'tester',
           _id: expect.any(String)
         });
       });
   });
 
-  it('gets a list of all people', () => {
-    return request(peopleApp)
-      .get('/people')
+  it('gets a list of people with the /people route', () => {
+    return People.create({
+      name: 'tester'
+    })
+      .then(() => {
+        return request(peopleApp)
+          .get('/people');
+      })
       .then(res => {
         expect(res.body).toHaveLength(1);
+        expect(res.body).toContainEqual({
+          name: 'tester',
+          _id: expect.any(String)
+        });
       });
   });
 
   it('gets a person by id', () => {
-    People.create({
-      name: 'tester',
-      age: 20,
-      color: 'purple'
+    return People.create({
+      name: 'tester'
     })
       .then(createdPerson => {
         return request(peopleApp)
@@ -46,10 +47,40 @@ describe('People routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           name: 'tester',
-          age: 20,
-          color: 'purple',
           _id: expect.any(String)
         });
       });
-  });
+  });  
+
+  it('updates a person by id', () => {
+    return People.create({
+      name: 'tesstter'
+    })
+      .then(createdPerson => {
+        return request(peopleApp)
+          .put(`/people/${createdPerson._id}`)
+          .send({ name: 'tester' });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'tester',
+          _id: expect.any(String)
+        });
+      });
+  });  
+
+  it('deletes a person by id', () => {
+    return People.create({
+      name: 'tester'
+    })
+      .then(createdPerson => {
+        return request(peopleApp)
+          .delete(`/people/${createdPerson._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
+        });
+      });
+  });  
 });
