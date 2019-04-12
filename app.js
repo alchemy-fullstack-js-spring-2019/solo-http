b91e59efe74ffeda2a55e736da7d7e9b71a3a488
Cara Ponzini <ponzini@gmail.com>
2019-04-11 17:23:53 -0700

    created body parser

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const { parse } = require('url');
+const uuid = require('uuid');
+const bodyParser = require('./lib/body-parser');
 
+const notes = {};
 module.exports = (req, res) => {
   const url = parse(req.url, { parseQueryString: true });
+  if(url.pathname === '/note' && req.method === 'POST') {
+    const id = uuid();
+    bodyParser(req)
+      .then(json => {
+        notes[id] = { ...json, id };
+        res.send(notes);
+      });
+  } else
   if(url.pathname === '/tester') {
     res.setHeader('Content-Type', 'application/json');  
     // res.setHeader('Content-Type', 'text/html');
