0fa596bab4b63d503a64d25bb12757770f34fd6d
Ben Parisot <me@benparisot.com>
2019-04-12 11:09:44 -0700

    update store.js with working methods

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const http = require('http');
 const { parse } = require('url');
 const app = require('./lib/app');
-//Create a server
-// http.createServer((req, res) => {
-//   console.log('request incoming!');
-//   const url = parse(req.url);
-//   console.log(url);
 
-//   switch(url.pathname) {
-//     case '/birthday':
-//       res.setHeader('Content-Type', 'text/html');
-//       res.end('<html><body><span style="color:red">Happy Birthday Fren</span></body></html>');
-//       break;
+
+http.createServer((req, res) => {
+  const url = parse(req.url);
+
+  switch(url.pathname) {
+    case '/birthday':
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><span style="color:red">Happy Birthday Fren</span></body></html>');
+      break;
     
-//     case '/json':
-//       res.setHeader('Content-Type', 'application/json');
-//       res.end(JSON.stringify({ 'firstName': 'Duckface', 'lastName': 'McLiverneck' }));
-//       break;
+    case '/json':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ 'firstName': 'Duckface', 'lastName': 'McLiverneck' }));
+      break;
 
-//     case '/waterfalls':
-//       res.setHeader('Content-Type', 'text/html');
-//       res.end('<html><body><span style="color:blue">| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>****</span></body></html>');
-//       break;
+    case '/waterfalls':
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><span style="color:blue">| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>| | |<br/>****</span></body></html>');
+      break;
 
-//     default:
-//       res.statusCode = 404;
-//       res.end(`Sorry buuuuut....:${res.statusCode}`);
-//       break;
-//   }
-// })
-//   .listen(7890);
+    default:
+      res.statusCode = 404;
+      res.end(`Sorry buuuuut....:${res.statusCode}`);
+      break;
+  }
+})
+  .listen(7890);
 
 
 
