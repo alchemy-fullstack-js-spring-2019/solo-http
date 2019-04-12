50c3b923d4bddd07deeeffd0ddaa53a5e4b94f46
Cheri Portman <cheriportman@hotmail.com>
2019-04-12 14:31:57 -0700

    GET test passes

--- test/app.test.js
+++ test/app.test.js
@@ -1,34 +1,37 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const People = require('../lib/models/People');
 
 describe('app routes', () => {
-  // it('responds to the birthday route', () => {
-  //   return request(app)
-  //     .get('/birthday')
-  //     .then(res => {
-  //       expect(res.text).toEqual('Happy Birthday');
-  //     });
-  // });
-  // it('handles a json response', () => {
-  //   return request(app)
-  //     .get('/tester')
-  //     .then(res => {
-  //       expect(res.body).toEqual({ name: 'stitch' });
-  //     });
-  // });
-  // it('handles a query string', () => {
-  //   return request(app)
-  //     .get('/you')
-  //     .query({ name: 'cheri' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ text: 'hello cheri' });
-  //     });
-  // });
-  // it.only('gets a particular character from an api', () => {
-  //   return request(app)
-  //     .get('/character/2')
-  //     .then(res => {
-  //       expect(res.body).toEqual('Morty Smith');
-  //     });
+  afterAll(() => {
+    return People.drop();
+  });
+
+  it('POSTS a person to /people', () => {
+    return request(app)
+      .post('/people')
+      .send({ name: 'cu', age: 12, color: 'beige' })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'cu',
+          age: 12,
+          color: 'beige',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('GETS a list of all people', () => {
+    return request(app)
+      .get('/people')
+      .then(res=> {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+  // it('GETS a person by id', () => {
+  //   People.create({ name: 'tester', age: 100, color: 'blue' })
+  //     .then(createdPerson => {
+  //       return request(app)
+  //         .get('/people/${createdPerson._id')
+  //     })
   // });
 });
