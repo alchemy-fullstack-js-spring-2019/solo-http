f4d48988c05470811d4c670f89023610791ebdca
Payton Lower <paytown@me.com>
2019-04-12 14:32:04 -0700

    both tests passing

--- test/app-routing.test.js
+++ test/app-routing.test.js
@@ -1,22 +1,27 @@
 const request = require('supertest');
 const appRouting = require('../lib/app-routing');
+const People = require('../lib/models/People');
 
 // jest.mock('../lib/service/rickAndMortyApi.js');
 
 describe('app routes', () => {
-  // it('creates a person with /people', () => {
-  //   return request(appRouting)
-  //     .post('/people')
-  //     .send({ name: 'ryan', age: 32, color: 'red' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         name: 'ryan',
-  //         age: 32,
-  //         color: 'red',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  afterAll(() =>{
+    return People.drop();
+  });
+
+  it('creates a person with /people', () => {
+    return request(appRouting)
+      .post('/people')
+      .send({ name: 'ryan', age: 32, color: 'red' })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'ryan',
+          age: 32,
+          color: 'red',
+          _id: expect.any(String)
+        });
+      });
+  });
   
   it('returns a list of people', () => {
     return request(appRouting)
