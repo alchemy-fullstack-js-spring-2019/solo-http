e93c009dcea320375332c6b44f6f9f50453f1b17
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-11 17:28:01 -0700

    end of day

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const http = require('http');
 const app = require('./lib/app.js');
 const appJson = require('./lib/app.json.js');
-const request = require('supertest');
+
 
 app.listen(7890);
 appJson.listen(4567);
---
+++ test/app.characters.test.js
@@ -0,0 +1,8 @@
+// const appChar = require('../lib/app.characters.js');
+// const request = require('supertest');
+
+describe('Rick and Morty Service tests', () => {
+  it('makes a request to rick and morty', () => {
+
+  });
+});
