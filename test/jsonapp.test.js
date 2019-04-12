31df6131c4ad43a3fd68f9e2ebb8d2c191cc09d2
Laura Badgley <l.badgleyy@gmail.com>
2019-04-11 17:45:08 -0700

    commit

---
+++ test/jsonapp.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../lib/jsonapp');
+
+describe('test should route path', () => {
+  it('should respond to tester ', () => {
+    return request()
+  });
+});
