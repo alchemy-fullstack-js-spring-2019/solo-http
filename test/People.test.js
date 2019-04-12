36bfbc92d995b9f235bb230076165a833e31c574
Anna Ridenour <annamridenour@gmail.com>
2019-04-12 14:34:31 -0700

    started get/find, test isn't passing tho

--- test/People.test.js
+++ test/People.test.js
@@ -1,8 +1,12 @@
 const app = require('../lib/app');
 const request = require('supertest');
+const People = require('../lib/models/People');
 
-describe('', () => {
-    it('', () => {
+describe('app routes', () => {
+    afterAll(() => {
+        return People.drop();
+    });
+    it('creates', () => {
         return request(app)
             .post('/people')
             .send({ name: 'jared', age: 72, color: 'glitter' })
@@ -10,4 +14,29 @@ describe('', () => {
                 expect(res.body).toEqual({ name: 'jared', age: 72, color: 'glitter', _id: expect.any(String) });
             });
     });
+
+    it('gets a list of all people', () => {
+        return request(app)
+            .get('/people')
+            .then(res => {
+                expect(res.body).toHaveLength(1);
+            });  
+    });
+
+    // it('gets a person by id', () => {
+    //     People.create({ name: 'tester', age: 100, color: 'blue' })
+    //         .then(createdPerson => {
+    //             return request(app)
+    //                 .get(`/people/${createdPerson._id}`);
+    //         })
+    //         .then(res => {
+    //             expect(res.body).toEqual({
+    //                 name: 'tester',
+    //                 age: 100,
+    //                 color: 'blue',
+    //                 _id: expect.any(String)
+    //             });
+    //         });
+
+    // })
 });
