95784fb823fadac0ddd3a866a229123bd9b32bb4
Cara Ponzini <ponzini@gmail.com>
2019-04-12 17:44:46 -0700

    only one failing test for kittens

--- test/app.test.js
+++ test/app.test.js
@@ -1,33 +1,39 @@
 const request = require('supertest');
-const app = require('../lib/app');
-const People = require('../lib/models/People');
+const app = require('../lib/app.js');
+const Kittens = require('../lib/models/Kittens');
 
 
 describe('app routes', () => {
   afterEach(() => {
-    return People.drop();
+    return Kittens.drop();
   });
 
-  test('it creates a person with the /people route (POST)', () => {
-    const person = {
-      name: 'cara',
-      age: '35',
+  test('it creates a kitten with the /kittens route (POST)', () => {
+    const kitten = {
+      name: 'fluffy',
+      age: '5',
       color: 'black'
     };
     return request(app)
-      .post('/people').send(person)
+      .post('/kittens')
+      .send(kitten)
       .then(res => {
-        expect(res.body).toEqual({ ...person, _id: expect.any(String) });
+        expect(res.body).toEqual({ 
+          name: 'fluffy',
+          age: '5',
+          color: 'black', 
+          _id: expect.any(String) 
+        });
       });
   });
 
-  test('gets a list of all people with /people', () => {
-    return People.create({
+  test('gets a list of all kittens with /kittens', () => {
+    return Kittens.create({
       name: 'tester'
     })
       .then(() => {
         return request(app)
-          .get('/people');
+          .get('/kittens');
       })
       .then(res => {
         expect(res.body).toHaveLength(1);
@@ -35,26 +41,29 @@ describe('app routes', () => {
   });
 
   test('gets a person by id', () => {
-    return People.create({
+    return Kittens.create({
       name: 'tester'
     })
-      .then(person => {
+      .then(kitten => {
         return request(app)
-          .get(`/people/${person._id}`);
+          .get(`/kittens/${kitten._id}`);
       })
       .then(res => {
-        expect (res.body).toEqual({ name: 'tester', _id: expect.any(String) });
+        expect (res.body).toEqual({
+          name: 'tester', 
+          _id: expect.any(String) 
+        });
       });
   });
 
-  test('it updates person by id', () => {
-    return People.create({ name: 'testr' })
-      .then(person => {
+  test('it updates kitten by id', () => {
+    return Kittens.create({ name: 'testr' })
+      .then(kitten => {
         return request(app)
-          .put(`/people/${person._id}`).send({ name: 'cara' })
+          .put(`/kittens/${kitten._id}`).send({ name: 'fluffy' })
           .then(res => {
             expect(res.body).toEqual({
-              name: 'cara',
+              name: 'fluffy',
               _id: expect.any(String)
             });
           });
@@ -62,10 +71,10 @@ describe('app routes', () => {
   });
 
   test('Deletes a thing by ID', () => {
-    return People.create({ name: 'test' })
-      .then(person => {
+    return Kittens.create({ name: 'test' })
+      .then(kitten => {
         return request(app)
-          .delete(`/people/${person._id}`);
+          .delete(`/kittens/${kitten._id}`);
       })
       .then(res => {
         expect(res.body).toEqual({
@@ -73,17 +82,5 @@ describe('app routes', () => {
         });
       });
   });
-
-
-
-  // it('tester pathname resolves ', () => {
-  //   return request(app).get('/tester')
-  //     .then(res => expect(res.body).toEqual({ testing: '123' }));
-  // });
-
-  // it('resolves to object', () => {
-  //   return request(app).get('/you?name=cara')
-  //     .then(res => expect(res.body).toEqual({ text: 'hi there cara' }));
-  // });
 });
 
