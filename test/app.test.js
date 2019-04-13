21f6cddb58ac7bb699a9c20ea1f2ba5fd06dab77
ollinieminen <otn987@gmail.com>
2019-04-13 13:06:04 -0700

    start refactoring

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,14 @@
 const request = require('supertest');
 const app = require ('../lib/app');
+const People = require('../lib/models/people');
 
-describe.only('app routes', ()=>{
-  it('responds to the bday route', () => {
-    return request(app)
-      .get('/tester')
-      .then(res=>{
-        expect(res.text).toEqual('testing123');
-      });
+describe('app routes', () => {
+  afterAll(() => {
+    return People.drop();
   });
-});
+
+  describe('creates person with /people', ()=>{
+    return request(app)
+    .post('/people')
+    .send({})
+  });
