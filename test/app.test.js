9d143b444499a87f8a535f2dd6501f4d40111d9a
ollinieminen <otn987@gmail.com>
2019-04-14 15:41:16 -0700

    another test passes

--- test/app.test.js
+++ test/app.test.js
@@ -3,12 +3,57 @@ const app = require ('../lib/app');
 const People = require('../lib/models/people');
 
 describe('app routes', () => {
-  afterAll(() => {
+  afterEach(() => {
     return People.drop();
   });
 
-  describe('creates person with /people', ()=>{
+  it('creates a perfson with /peope;', ()=>{
     return request(app)
-    .post('/people')
-    .send({})
+      .post('/people')
+      .send({ name: 'test' })
+      .then(res=>
+      {   
+        return expect(res.body).toEqual({ name:'test', _id: expect.any(String) });
+      }
+      );
   });
+  it('it finds a user with id', ()=>{
+    return request(app)
+      .post('/people/')
+      .send({ name: 'test' })
+      .then((res=>{
+        return request(app)
+          .get(`/people/?id=${res.body._id}`);
+      }))
+      .then(foundPerson=>{
+        expect(foundPerson.body).toEqual({ name: 'test', _id: expect.any(String) });
+      });
+  });
+  it('finds all users', ()=>{
+    return request(app)
+      .post('/people')
+      .send({ name:'jimbob' })
+      .then(()=>{
+        return request(app)
+          .get('/people')
+          .then(people=>{
+            expect(people.body).toContainEqual({ name:'jimbob', _id: expect.any(String) });
+          });
+      });
+  });
+  it('fins a person and updates', ()=>{
+    return request(app)
+      .post('/people')
+      .send({ name:'Wrong Name' })
+      .then((newPerson)=>{
+        return request(app)
+          .put(`/people/?id=${newPerson.body._id}`)
+          .send({ name: 'Right Name' })
+          .then(res=>{
+            return expect(res.body).toEqual({ name:'Right Name', _id: expect.any(String) });
+          });
+          
+      });
+  });
+});
+
