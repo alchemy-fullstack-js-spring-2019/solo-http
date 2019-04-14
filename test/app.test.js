b510dd4b1ed9800bd00ff2339b69b6bc927256c5
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-14 16:10:09 -0700

    passing app tests with refined methods and routing handling

--- test/app.test.js
+++ test/app.test.js
@@ -18,4 +18,58 @@ describe('app routes', () => {
                 });
             });
     });
+    it('gets a list of people with the /people route', () => {
+        return People.create({
+            name: 'Brain'
+        })
+            .then(() => {
+                return request(app)
+                    .get('/people');
+            })
+            .then(response => {
+                expect(response.body).toHaveLength(1);
+                expect(response.body).toContainEqual({
+                    name: 'Brain',
+                    _id: expect.any(String)
+                });
+            });
+    });
+    it('gets a person by id', () => {
+        return People.create({ name: 'Colin' })
+            .then(person => {
+                return request(app)
+                    .get(`/people/${person._id}`);
+            })
+            .then(response => {
+                expect(response.body).toEqual({
+                    name: 'Colin',
+                    _id: expect.any(String)
+                });
+            });
+    });
+    it('updates a person by id', () => {
+        return People.create({ name: 'Test' })
+            .then(person => {
+                return request(app)
+                    .put(`/people/${person._id}`)
+                    .send({ name: 'Brian' });
+            })
+            .then(response => {
+                expect(response.body).toEqual({
+                    name: 'Brian'
+                });
+            });
+    });
+    it('deletes a person by id', () => {
+        return People.create({ name: 'harver'})
+            .then(person => {
+                return request(app)
+                    .delete(`/people/${person._id}`); 
+            })
+            .then(response => {
+                expect(response.body).toEqual({
+                    deleted: 1
+                });
+            });
+    });
 });
