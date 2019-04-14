e413050a306e4a5e2c2bde254f76d7fd1ff0ae24
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-14 15:11:13 -0700

    resetting file structure and getting things ready for monday

--- server.js
+++ server.js
@@ -1,28 +1,5 @@
 const app = require('./lib/app');
 const { rickNMorty } = require('./lib/services/rickAndMorty');
-// const mortyServer = require('./lib/services/rickAndMorty');
-// http.createServer((request, response) => {
-//     const url = parse(request.url);
-//     console.log('client connected...' + url);
-//     response.setHeader('Content-Type', 'text/plain');
-//     response.end('Hello All');
-// }).listen(3000);
-
-// http.createServer((request, response) => {
-//     const url = parse(request.url);
-//     console.log('client connected...' + url);
-//     response.setHeader('Content-Type', 'text-html');
-//     response.end(`
-//     <html>
-//         <head>
-//             <title>WORLD WIDE WEB</title>
-//         </head>
-//             <body>
-//                 <h1>Thanks for visiting!</h1>
-//             </body>
-//     </html>
-//     `);
-// }).listen(3000);
 
 app.listen(3000);
 rickNMorty.listen(4000);
